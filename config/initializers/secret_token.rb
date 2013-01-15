da6858bdc58b5af332fa8e5761af94d4daeb8175
Austin <williamaustinhinkle@gmail.com>
2013-01-14 22:24:58 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gastroverse::Application.config.secret_token = 'e8a9ffa56749855fa093dc012e603ab7dd9fa74b896f206873d8a07512e4a8aeaf71e9b79c095ae709ab3805f05136719dae7b6f40bd586e35a491a78c6df52a'
