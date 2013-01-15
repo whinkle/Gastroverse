b96f77ddde78f7ad6b442781a525984f5063a6c5
Austin <williamaustinhinkle@gmail.com>
2013-01-14 22:57:49 -0500

    Add LandingPage and StaticPages controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Gastroverse::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/help"
+
+  get "landing_page/home"
+
+  get "landing_page/about"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
